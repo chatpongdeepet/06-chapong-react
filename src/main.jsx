201198bd341d594e83399dbd357683a3106a1cca
Chatpong Deepet <chatpond@outlook.com>
2024-11-06 10:55:00 +0700

    initial project folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from './pages/Home'
+import Owner from './pages/Owner'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/owner" element={<Owner />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
