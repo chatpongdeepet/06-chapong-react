b853124a43d0ebff1d4b2e415fec72fe540ee677
Chatpong Deepet <chatpond@outlook.com>
2024-11-06 15:26:31 +0700

    Change profile image

--- src/pages/Owner.jsx
+++ src/pages/Owner.jsx
@@ -8,7 +8,7 @@ const Owner = () => {
         <h1 className="text-2xl font-semibold">Chatpong Deepet</h1>
         <img
           className="rounded-full mx-auto w-[150px] m-10"
-          src="../../public/profile.icon.jpg"
+          src="https://i.pinimg.com/236x/22/09/02/220902e0b406bbd28afccd44a3551b1e.jpg"
           alt="owner-profile-shrek-"
         />
         <p>Hello coding</p>
